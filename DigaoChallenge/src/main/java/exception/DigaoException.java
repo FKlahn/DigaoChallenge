b61bdf8f7b5426f2d1289648ae860ca1c3d9852d
Rodrigo Junior Marques Zamora <143302@queroquero.local>
2022-09-22 18:09:33 -0300

    Adicionada opção 5, pedra papel ou tesoura!

--- DigaoChallenge/src/main/java/exception/DigaoException.java
+++ DigaoChallenge/src/main/java/exception/DigaoException.java
@@ -1,6 +1,9 @@
 package exception;
 
 public class DigaoException extends RuntimeException {
+    public DigaoException(String message) {
+        super(message);
+    }
     public DigaoException() {
         super();
     }
